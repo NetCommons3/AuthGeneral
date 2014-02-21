362160d42ec5cec9bca4554bafa061c6f6345a1a
topaz2 <topaz2.3333@gmail.com>
2014-02-21 10:14:44 +0900

    Added all test

---
+++ Test/Case/AllAuthGeneralTest.php
@@ -0,0 +1,9 @@
+<?php
+class AllAuthGeneralTest extends CakeTestSuite {
+
+	public static function suite() {
+		$suite = new CakeTestSuite('All tests');
+		$suite->addTestDirectoryRecursive(TESTS . 'Case');
+		return $suite;
+	}
+}
