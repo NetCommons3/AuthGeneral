0da9b67d9d07b61280ea85d372b80207342e6553
topaz2 <topaz2.3333@gmail.com>
2014-02-28 18:39:22 +0900

    Added mock

---
+++ Controller/AuthGeneralController.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppController', 'Controller');
+App::uses('AuthController', 'Auth.Controller');
+/**
+ * AuthGeneral Controller
+ *
+ * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
+ * @link     http://www.netcommons.org NetCommons Project
+ * @license  http://www.netcommons.org/license.txt NetCommons License
+ */
+class AuthGeneralController extends AuthController {
+}
