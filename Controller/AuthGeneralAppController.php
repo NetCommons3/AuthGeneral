53626e43b7c9a6c0d6163a06a5e3b2387ff8554d
topaz2 <topaz2.3333@gmail.com>
2014-03-27 17:33:57 +0900

    Refactored authenticator switching feature

--- Controller/AuthGeneralAppController.php
+++ Controller/AuthGeneralAppController.php
@@ -3,14 +3,4 @@
 App::uses('AuthController', 'Auth.Controller');
 
 class AuthGeneralAppController extends AuthController {
-
-/**
- * Return authentication adapter name
- *
- * @return string Authentication adapter name
- * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
- **/
-	public function getAuthenticator() {
-		return 'Form';
-	}
 }
